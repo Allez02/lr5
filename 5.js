1d4d09fe9f67b3af07ca66289d46adb6dc0b2f9b
Allez02 <74099015+Allez02@users.noreply.github.com>
2021-01-23 12:47:48 +0300

    Add files via upload

--- 5.js
+++ 5.js
@@ -1,3 +1,4 @@
+"use strict"
 //Задание 1
 function a() {
 	alert( " Моськин Иван Александрович " );
@@ -9,9 +10,9 @@ function as() {
 }
 //Задание 3
 function asd() {
-	var 
-	pole1=document.getElementById('pole1'); 
-	pole2=document.getElementById('pole2');
+	 
+	 var pole1=document.getElementById('pole1'); 
+	var  pole2=document.getElementById('pole2');
 	if (pole1.value=='') {
 		alert("Введите текст в первое поле!");
 	}
@@ -20,11 +21,11 @@ function asd() {
 	}
 }
 function clasd(){
-	var 
-	pole1=document.getElementById('pole1'); 
-	pole1.value='';
-	pole2=document.getElementById('pole2');
-	pole2.value='';
+	 
+	 pole1=document.getElementById('pole1'); 
+	 pole1.value='';
+	 pole2=document.getElementById('pole2');
+	 pole2.value='';
 
 }
 //Задание 4
@@ -32,14 +33,14 @@ function clasd(){
 
 function table() {
 	var a=document.getElementById('row').value;
-	b=document.getElementById('col').value;
-		tbody = document.getElementById('form1');
+	var b=document.getElementById('col').value;
+	var tbody = document.getElementById('form1');
 	if (a>=1,b>=1) {
 		tbody = document.getElementById('table1');
 			for (i=1;i<=a;i++) {
-				row = document.createElement("TR");
+				var row = document.createElement("TR");
 				for (j=1;j<=b;j++) {
-					td1 = document.createElement("TD");
+				var	td1 = document.createElement("TD");
 					td1.appendChild(document.createTextNode(Math.round(Math.random() * 10)));
 					row.appendChild(td1);
 					tbody.appendChild(row);
@@ -54,17 +55,17 @@ function table() {
 }
 //Задание 5
 function result() {
-	var edic=document.getElementById('answer'); res=0;
-	znak=edic.value.slice(edic.value.indexOf(' ')+1,edic.value.indexOf(' ')+2);
-	numone=edic.value.slice(0 , edic.value.indexOf(' '));
-	numdva=edic.value.slice(edic.value.indexOf(' ') + 2,edic.value.length );
+	var edic=document.getElementById('answer');var res=0;
+	var znak=edic.value.slice(edic.value.indexOf(' ')+1,edic.value.indexOf(' ')+2);
+	var numone=edic.value.slice(0 , edic.value.indexOf(' '));
+	var numdva=edic.value.slice(edic.value.indexOf(' ') + 2,edic.value.length );
 	if (znak=='*') {
 		res=Number(numdva) * Number(numone);edic.value=res;
 	}
 	else {
 		if (znak == '/') {
 			if (Number(numdva)==0) {
-				alert('Так низя');
+				alert('На ноль делить нельзя');
 				cleand();
 			}
 			else{
